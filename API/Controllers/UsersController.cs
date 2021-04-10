a28bd7356aeed63cb95213cf9e89893d0a3f3960
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-04-10 23:30:36 +0200

    Added user controller and updated code

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,7 @@
+using System.Threading.Tasks;
 using API.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -9,6 +11,13 @@ namespace API.Controllers
         public UsersController(DataContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("getUsers")]
+        public async Task<ActionResult> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
         }
     }
 }
