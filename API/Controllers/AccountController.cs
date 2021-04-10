c6542d98e7019179bec50f574f9f6720f4e24d33
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-04-10 23:18:32 +0200

    Created controllers for register and to get users

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,7 +1,39 @@
+using System.Threading.Tasks;
+using API.Data;
+using API.Dtos;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
 namespace API.Controllers
 {
-    public class AccountController
+    public class AccountController : ControllerBase
     {
-        
+        private readonly DataContext _context;
+
+        public AccountController(DataContext context)
+        {
+            _context = context;
+
+        }
+
+        [HttpPost("register")]
+
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            var user = new AppUser
+            {
+                Username = registerDto.Username.ToLower(),
+                Password = registerDto.Password
+            };
+
+            _context.Users.Add(user);
+
+            await _context.SaveChangesAsync();
+
+            return new UserDto
+            {
+                Username = user.Username,
+            };
+        }
     }
 }
--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,7 +1,12 @@
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
+
 namespace API.Controllers
 {
-    public class UsersController
+    public class UsersController : ControllerBase
     {
-        
+        public UsersController(DataContext context)
+        {
+        }
     }
 }
