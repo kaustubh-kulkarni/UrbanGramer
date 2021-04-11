93c2049d82fd0debff7cdb5a729eed5e4aa79647
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-04-11 17:09:25 +0200

    Updated login logic

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -29,6 +29,15 @@ namespace API.Controllers
 
             if(user == null) return Unauthorized("Invalid Username");
 
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+            }
+
             return user;
         }
 
