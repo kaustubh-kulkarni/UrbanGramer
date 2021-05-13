1839a64318f5cfae068cb7cd513dbc19c2221610
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-05-13 11:28:04 +0200

    Added post by username logic in frontend client side

--- client/src/app/components/posts/post-edit/post-edit.component.ts
+++ client/src/app/components/posts/post-edit/post-edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs/operators';
+import { Post } from 'src/app/_models/post';
+import { User } from 'src/app/_models/user';
+import { AccountService } from 'src/app/_services/account.service';
+import { PostService } from 'src/app/_services/post.service';
 
 @Component({
   selector: 'app-post-edit',
@@ -6,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-edit.component.css']
 })
 export class PostEditComponent implements OnInit {
-
-  constructor() { }
+  user: User;
+  post: Post;
+  constructor(private accountService: AccountService ,private postService: PostService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user =>this.user = user);
+   }
 
   ngOnInit(): void {
+    this.loadUserPosts();
+  }
+
+  loadUserPosts(){
+    this.postService.getPost(this.user.username).subscribe(res => {
+      this.post = res;
+    })
   }
 
 }
